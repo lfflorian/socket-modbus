72d3fbdc61de217977383dcf9bbdad49ea807b80
luis <lfflorian4@gmail.com>
2019-07-01 11:22:24 -0600

    Modbus

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var config = require('./config');
-
-
-server.listen(config.port, function(socket) {
-	console.log(`Servidor corriendo en ${config.port}`);
+var net = require('net');
+var modbus = require('modbus-tcp');
+var modbusServer = new modbus.Server();
+const port = 502;
+var tcpServer = net.createServer();
+tcpServer.listen(port,function(){
+    console.log('TCP Socket bound to port '+port);
 });
-
-io.on('connection', function(socket) {
-    console.log('Un cliente se ha conectado');
-    
-    socket.on('disconnect', function(){
-        console.log("Usuario desconectado");
-    })
-
-    socket.on('message', function () {
-        console.log('Hemos recibido un mensaje: ' + message);
+tcpServer.on('connection', function(socket){
+    console.log('client has connected');
+    modbusServer.pipe(socket);
+    socket.on('error', function(e){
+        console.log('Connection error: '+e);
+        socket.destroy();
     });
-    //socket.emit('messages', messages);
+    socket.on('close', function(e){
+        console.log('Client has closed connection.');
+    });
 });
+modbusServer.on('read-holding-registers', readHoldingRegisters);
+modbusServer.on('read-coils',readCoils);
+modbusServer.on('write-multiple-registers',writeRegisters);
+function readHoldingRegisters(from,to,reply) {
+    console.log('Read holding registers '+from+'-'+to);
+    var values = [1,6,3,9]; // sample values just to see if it works.
+    return reply(null,bufferify(values));
+}
+function readCoils(from,to,reply) {
+    console.log('Read coils '+from+'-'+to);
+    var values = [2,0,8]; // anything greater than zero is received as a 1
+    return reply(null,values);
+}
+function writeRegisters(from,to,items,reply) {
+    console.log('Write registers '+from+'-'+to);
+    console.log('  items:'+items);
+    reply();
+}
+function bufferify(itemsArray) {
+    // When client reads values, have to supply an 
+    // array of Buffers (not just an array of numbers) to the reply function.
+    var n = itemsArray.length;
+    var registers = [];
+    for (var i=0; i<n; i++) {
+        registers[i] = Buffer.alloc(2);
+        registers[i].writeInt16BE(itemsArray[i],0);
+    }
+}
