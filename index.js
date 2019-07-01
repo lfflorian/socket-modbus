6892fb72dbc3631252581c401f8bb66c7ef2285d
luis <lfflorian4@gmail.com>
2019-06-30 23:37:57 -0600

    socket

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-var http = require('http');
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 var config = require('./config');
-var ip = require('ip');
 
-const requestHandler = (request, response) => {
-    console.log(request.url)
-    response.end('Hello Node.js Server!')
-}
 
-const server = http.createServer(requestHandler)
+server.listen(config.port, function(socket) {
+	console.log(`Servidor corriendo en ${config.port}`);
+});
 
-server.listen(config.port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err)
-    }
-    console.log(`ip address is: ${ip.address()} `)
-    console.log(`server is listening on ${config.port}`)
-})
+io.on('connection', function(socket) {
+    console.log('Un cliente se ha conectado');
+    
+    socket.on('disconnect', function(){
+        console.log("Usuario desconectado");
+    })
+
+    socket.on('message', function () {
+        console.log('Hemos recibido un mensaje: ' + message);
+    });
+    //socket.emit('messages', messages);
+});
